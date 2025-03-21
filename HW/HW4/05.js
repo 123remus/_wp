dce287010d14646b7c3b7314d7f2ac5143ac07b4
123remus <remus0304@gmail.com>
2025-03-21 11:32:01 +0800

    add HW4

---
+++ HW/HW4/05.js
@@ -0,0 +1,15 @@
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+  }
+  
+  class Dog extends Animal {
+    speak() {
+      return `Woof! I am ${this.name}`;
+    }
+  }
+  
+  const dog = new Dog("Buddy");
+  console.log(dog.speak()); 
+  
