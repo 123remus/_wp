1d64ed3a3d6aa6b75c09d161f8a3d2028bb34ef6
123remus <remus0304@gmail.com>
2025-03-24 15:28:40 +0800

    add HW3

--- HW/HW3/4.js
+++ HW/HW3/4.js
@@ -1,7 +1,7 @@
- let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let nums2=[];
 for(let i=0;i<nums.length;i++){
-    if(nums[i]%2==0){
+    if(nums[i]%2!=0){
         nums2.push(nums[i]);
         
     }
